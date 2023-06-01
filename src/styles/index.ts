896418e0a7e98e88a1d5a044a2c6d5b248be4658
Santiago Negreira <santiago.negreira@gesplan.com.br>
2023-06-01 13:18:25 -0300

    Create global styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -12,7 +12,14 @@ export const {
  } = createStitches({
     theme: {
         colors: {
-            rocketseat: '#8257e6',
+            white: '#FFFFFF',
+            gray900: '#121214',
+            gray800: '#202024',
+            gray300: '#C4C4CC',
+            gray100: '#E1E1E6',
+
+            green500: '#00875f',
+            green300: '#00b37e'
         }
     }
 })
