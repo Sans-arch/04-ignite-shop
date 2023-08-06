b086dd458ed20ec01a973c92651b9d9a2fe9f13e
Santiago Negreira <sansfleitas@gmail.com>
2023-08-06 13:51:20 -0300

    Use the Image component of NextJS
    - Useful to optimize images

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import type { AppProps } from 'next/app'
-import { globalStyles } from '../styles/global'
+import type { AppProps } from 'next/app';
+import Image from 'next/image';
+import { globalStyles } from '../styles/global';
 
 import logoImg from '../assets/logo.svg';
 import { Container, Header } from '../styles/pages/app';
@@ -10,7 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg} alt="" />
       </Header>
 
       <Component {...pageProps} />
