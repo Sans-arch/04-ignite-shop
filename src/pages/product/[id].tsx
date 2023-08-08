6d6912fa6fcdc24470237b28c50ce886784a817e
Santiago Negreira <sansfleitas@gmail.com>
2023-08-07 21:07:58 -0300

    Create Product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,30 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/src/styles/pages/product";
 import { useRouter } from "next/router";
 
 export default function Product() {
-    const { query } = useRouter();
-    
-    return (
-        <h1>Product: {JSON.stringify(query)}</h1>
-    );
+  const { query } = useRouter();
+
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+          culpa ex quo quas ducimus? In quisquam deleniti possimus est hic totam
+          laboriosam quae quam! Sapiente ratione corporis sit perspiciatis
+          repellendus.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
